566f6120a0055d8d908628e6045408519937e78a
evgeniagotskina <evgeniagotskina@yandex.ru>
2022-02-24 13:48:13 +0300

    функции с исправлениями

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 // Функция генерации случайных чисел от 0 до 100
 function randomNumber(min, max){
-  if (max>min) {
+  if (max>min && min>=0) {
     const number = Math.random()*(max-min) + min;
     return Math.floor(number);
   }
-  console.log('Ошибка!Максимальное число меньше минимального.');
+  return ('Ошибка!Максимальное число меньше минимального.');
 }
 randomNumber(0,9);
 //Функция сравнения длины строки с максимальной длиной
 function comparLengthString (string, stringMax) {
-  if (string.length < stringMax) {
-    return(true);
-  }
-  else{
-    return(false);
-  }
+  return(string.length <= stringMax);
 }
+comparLengthString ();
